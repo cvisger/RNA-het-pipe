dfaa4c55df63bfe1f5dd3ffcd34fc1de60d73760
Clayton Visger <cvisger@users.noreply.github.com>
2018-01-28 10:11:14 -0800

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -2,5 +2,5 @@
 conda install -c bioconda sra-tools -y
 conda install fastqc -y
 conda install trimmomatic -y
-conda install -c bioconda gmap 
 conda install freebayes -y
+conda install -c bioconda gmap -y
