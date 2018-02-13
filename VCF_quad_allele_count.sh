03cfee577fc558e9eb89832738615adb6047cdc3
mike <michael_chester@hotmail.co.uk>
2018-02-13 17:08:37 +0000

    Added VCF_quad_allele_count.sh

---
+++ VCF_quad_allele_count.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#bash script to print off '0/1/2/3' haplotype counts from VCF 
+# usage: bash VCF_haplotype_count.sh INPUT.vcf 
+
+
+input_file="${1}"
+declare -a LG_list=(LG1 LG2 LG3 LG4 LG5 LG6 LG7) 
+
+for LG in "${LG_list[@]}"
+do
+	echo -n "Total number of quadra-allelic 0/1/2/3 sites on Scaffold "$LG":"
+	grep $LG  $input_file | grep '0/1/2/3'| wc -l
+	grep $LG  $input_file | grep '0/1/2/3'| awk '{print $2}'
+done
+
+for LG in "${LG_list[@]}"
+do
+	echo -n "Total number of quadra-allelic 1/2/3/4 sites on Scaffold "$LG":"
+	grep $LG  $input_file | grep '1/2/3/4'| wc -l
+	grep $LG  $input_file | grep '1/2/3/4'| awk '{print $2}'
+done
+
+exit 0
